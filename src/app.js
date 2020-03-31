ced7ee34c0f02fb51958e38d08f9022b79a4e8e2
Steven Burrows <steven@Stevens-MacBook-Air.local>
2020-03-31 18:44:55 +0100

    MVP done, no css

--- src/app.js
+++ src/app.js
@@ -1,8 +1,52 @@
 import Vue from 'vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+    new Vue ({
+        el: '#app',
+        data: {
+             convertionRates: {},
+             amount: 0,
+             selectedCurrency: "",
+             converted: 0,
+             exchangeWay: ""
+
+        },
+        mounted(){
+            this.fetchConversionRates()
+        },
+
+        computed: {
+            convertedAmount: function() {
+                console.log(this.selectedCurrency);
+                console.log(this.amount);
+                console.log(this.exchangeWay);
+                if (this.exchangeWay == "toEuro") {
+                    this.converted = this.amount / this.selectedCurrency
+                };
+                if (this.exchangeWay == "fromEuro") {
+                    this.converted = this.amount * this.selectedCurrency
+                };
+                
+                
+                
+                console.log(this.converted);
+                
+                return this.converted.toFixed(2)
+   //             return this.amount * this.selectedCurrency
+            }
+
+        },
+        methods: {
+            fetchConversionRates: function() {
+                fetch('https://api.exchangeratesapi.io/latest')
+                .then(response => response.json())
+                .then( currency => this.convertionRates = currency)
+                
+                
+            }
+        }
 
 
 
+    })
 })
